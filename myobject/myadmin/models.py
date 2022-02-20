d3a658e9b62637f2b1e1586a41fcb6cd48b5b039
lupengliang <1559540409@qq.com>
2022-02-20 22:52:39 +0800

    2022.02.20 添加后台用户表

--- myobject/myadmin/models.py
+++ myobject/myadmin/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     nickname = models.CharField(max_length=50)  # 昵称
     password_hash = models.CharField(max_length=100)  # 密码
     password_salt = models.CharField(max_length=50)  # 密码干扰值
-    status = models.IntegerField(default=1)  # 状态：1正常/2禁用/9删除
+    status = models.IntegerField(default=1)  # 状态：1正常/2禁用/6管理员/9删除
     create_at = models.DateTimeField(default=datetime.now)  # 创建时间
     update_at = models.DateTimeField(default=datetime.now)  # 修改时间
 
