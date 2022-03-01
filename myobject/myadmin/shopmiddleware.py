8635ff1b85266b6dfc0ae2809d67c7b6e6accf81
lupengliang <1559540409@qq.com>
2022-03-01 22:52:01 +0800

    2022.03.01 添加登录中间件-未完成

--- myobject/myadmin/shopmiddleware.py
+++ myobject/myadmin/shopmiddleware.py
@@ -1,3 +1,9 @@
+# 自定义中间类 （执行是否登录判断）
+from django.shortcuts import redirect
+from django.urls import reverse
+import re
+
+
 class ShopMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
@@ -5,14 +11,18 @@ class ShopMiddleware:
         print(ShopMiddleware)
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called
         path = request.path
-        print("url", path)
+        print("url:", path)
+
+        # 判断管理后台是否登录
+        # 定义后台不登录也可直接访问的url列表
+        urllist = ['/myadmin/login', '/myadmin/logout', '/myadmin/dologin']
+        # 判断当前请求url地址是否是以/myadmin开头,并且不在urllist中，才进行判断
+        if re.match(r'^myadmin', path):
+            # 判断是否登录
+            pass 27.28
+            # 重写向到登录页
+            return redirect(reverse("myadmin_login"))
 
         response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
         return response
